33abbacb16e2e07c2458dd4aa7d09aaab26b2312
SoohaMagdy <sooha.magdy@gmail.com>
2019-10-06 18:28:05 +0200

    uploading project

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#loading").fadeOut(2000, function () {
+        $("body").css("overflow-y", "auto")
+    })
+})
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 800) {
+        $("#btnUp").fadeIn(500);
+    }
+    else {
+        $("#btnUp").fadeOut(500);
+    }
+})
+$("#btnUp").click(function(){
+    $("body,html").animate({scrollTop:0},1000)
+})
+
+
+
+$("nav a").click(function () {
+    let aHref = $(this).attr("href");
+    let aOffset = $(aHref).offset().top;
+    $("body,html").animate({ scrollTop: aOffset }, 1000)
+})
